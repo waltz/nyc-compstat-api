0962044e63edefb28bfa1d32e291696ca566b2f9
CJ Bryan <christianbryan@gmail.com>
2016-09-27 17:43:10 -0700

    Filter data from saved cassettes.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,9 @@ require_relative '../environment'
 Bundler.require(:test)
 
 VCR.configure do |config|
-  config.cassette_library_dir = File.expand_path('./fixtures/vcr_cassettes', File.dirname(__FILE__))
+  config.cassette_library_dir = File.expand_path('./fixtures/cassettes', File.dirname(__FILE__))
   config.hook_into :webmock
+  config.filter_sensitive_data('<S3_ACCESS_KEY_ID>') { ENV['S3_ACCESS_KEY_ID'] }
+  config.filter_sensitive_data('<S3_SECRET_ACCESS_KEY>') { ENV['S3_SECRET_ACCESS_KEY'] }
+  config.filter_sensitive_data('<S3_BUCKET>') { ENV['S3_BUCKET'] }
 end
